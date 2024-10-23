15dbd1f80fb17e5b3b273df571c6e5d722f01e0d
John Chee <cheecheeo@gmail.com>
2024-10-23 00:13:18 -0700

    hspec boilerplate

---
+++ hspec.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+
+cabal test --test-show-details=direct --test-option=--format=progress
---
+++ test/AudiobookSpec.hs
@@ -0,0 +1,14 @@
+module AudiobookSpec where
+
+import Test.Hspec
+
+spec :: Spec
+spec = do
+  describe "Prelude" $ do
+    describe "read" $ do
+      it "can parse integers" $ do
+        read "10" `shouldBe` (10 :: Int)
+
+    describe "head" $ do
+      it "returns the first element of a list" $ do
+        head [23 ..] `shouldBe` 23
