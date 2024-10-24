5e0552f15e4709bd1be23e28e1ff2d485e52c140
John Chee <cheecheeo@gmail.com>
2024-10-24 11:02:09 -0700

    try to test andThenProcess in AudiobookSpec

--- test/AudiobookSpec.hs
+++ test/AudiobookSpec.hs
@@ -1,9 +1,20 @@
 module AudiobookSpec where
 
+import qualified System.Process
+
+import Audiobook
+
 import Test.Hspec
 
 spec :: Spec
 spec = do
+  describe "Audiobook" $ do
+    describe "andThenProcess" $ do
+      it "can run pwd then date" $ do
+        let pwd = System.Process.shell "pwd"
+        let date = System.Process.shell "date"
+        andThenProcess pwd date `shouldReturn` Nothing
+
   describe "Prelude" $ do
     describe "read" $ do
       it "can parse integers" $ do
