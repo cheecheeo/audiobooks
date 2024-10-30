40d1e1bc84eeb5798cb9ba663738ab0528e0f210
John Chee <cheecheeo@gmail.com>
2024-10-29 22:38:50 -0700

    implement more tests

--- test/AudiobookSpec.hs
+++ test/AudiobookSpec.hs
@@ -28,14 +28,18 @@ newtype FileString = FileString { getFile :: String }
   deriving (Show)
 
 instance Arbitrary FileString where
-  arbitrary = do
-    let parts = Test.QuickCheck.elements ["foo", "bar", "baz", "fee", "fie", "foe", "fum"]
-    let exts = Test.QuickCheck.elements ["c", "ogg", "h", "wav", "m4b", "mp3", "opus"]
-    p1 <- parts
-    p2 <- parts
-    p3 <- parts
-    ext <- exts
-    return . FileString $ ("/" ++ p1 ++ "/" ++ p2 ++ "/" ++ p3 ++ "." ++ ext)
+  arbitrary =
+    let parts = Test.QuickCheck.elements
+          [ "foo", "bar", "baz", "fee", "fie", "foe", "fum", "i",
+            "smell", "the", "roses", "and", "hear", "the", "lions", "roar" ]
+        exts = Test.QuickCheck.elements
+          ["c", "ogg", "h", "wav", "m4b", "mp3", "opus", "m4a"]
+    in do
+      p1 <- parts
+      p2 <- parts
+      p3 <- parts
+      ext <- exts
+      return . FileString . mconcat $ ["/" , p1, "/", p2, "/", p3, ".", ext]
 
 spec :: Spec
 spec = do
@@ -92,7 +96,6 @@ spec = do
     it "will create a process for one file" $ do
       audioFile <- Path.parseAbsFile "/foo/bar/baz.ogg"
       outFile <- Path.parseAbsFile "/foo/bar/fee.m4a"
-      -- print $ ffmpegConcatCommand (audioFile :| []) outFile
       ffmpegConcatCommand (audioFile :| []) outFile `shouldReturn` CreateProcess {cmdspec = ShellCommand "ffmpeg -i \"/foo/bar/baz.ogg\" -filter_complex \"[0:0]concat=n=1:v=0:a=1[outa]\" -map \"[outa]\" -acodec alac \"/foo/bar/fee.m4a\"", cwd = Nothing, env = Nothing, std_in = Inherit, std_out = Inherit, std_err = Inherit, close_fds = False, create_group = False, delegate_ctlc = False, detach_console = False, create_new_console = False, new_session = False, child_group = Nothing, child_user = Nothing, use_process_jobs = False}
     -- TODO don't need the tempdir here, not doing any writing, just generating CreateProcesses
     it "will create a process for audio_files" $ Path.IO.withSystemTempDir "AudiobookSpec" $ (\dir -> do
@@ -103,17 +106,16 @@ spec = do
         (expectationFailure "empty audioFiles")
         (\fs -> ffmpegConcatCommand fs outFile `shouldReturn` CreateProcess {cmdspec = ShellCommand "ffmpeg -i \"/home/chee1/packages/audiobooks/audio_files/Columbia-dx1536-cax10357.ogg\" -i \"/home/chee1/packages/audiobooks/audio_files/NordwindSonne.wav\" -i \"/home/chee1/packages/audiobooks/audio_files/Handel_-_messiah_-_02_comfort_ye.ogg\" -i \"/home/chee1/packages/audiobooks/audio_files/Handel_-_messiah_-_44_hallelujah.ogg\" -filter_complex \"[0:0][1:0][2:0][3:0]concat=n=4:v=0:a=1[outa]\" -map \"[outa]\" -acodec alac \"/foo/bar/out.m4a\"", cwd = Nothing, env = Nothing, std_in = Inherit, std_out = Inherit, std_err = Inherit, close_fds = False, create_group = False, delegate_ctlc = False, detach_console = False, create_new_console = False, new_session = False, child_group = Nothing, child_user = Nothing, use_process_jobs = False})
         (Data.List.NonEmpty.nonEmpty audioFiles)
-      -- print $ ffmpegConcatCommand (audioFile :| []) outFile
-      --ffmpegConcatCommand (audioFile :| []) outFile `shouldBe` CreateProcess {cmdspec = ShellCommand "ffmpeg -i \"/foo/bar/baz.ogg\" -filter_complex \"[0:0]concat=n=1:v=0:a=1[outa]\" -map \"[outa]\" -acodec alac \"/foo/bar/fee.m4a\"", cwd = Nothing, env = Nothing, std_in = Inherit, std_out = Inherit, std_err = Inherit, close_fds = False, create_group = False, delegate_ctlc = False, detach_console = False, create_new_console = False, new_session = False, child_group = Nothing, child_user = Nothing, use_process_jobs = False}
       )
 
   describe "makeM4bFromDir" $ do
     it "can make a file from ./audio_files" $ do
       audioFilesDirectory <- (</>) <$> Path.IO.getCurrentDir <*> (Path.parseRelDir "./audio_files")
-      -- (evalIO . makeM4bFromDir $ audioFilesDirectory) `shouldReturn` True
-      pending
-    it "can make a file from <a single audio file>" $ do
-      pending
+      (evalIO . makeM4bFromDir $ audioFilesDirectory) `shouldReturn` True
+    it "can make a file from ./audio_files/Handel_-_messiah_-_44_hallelujah.ogg" $ do
+      audioFileDirectory <- (</>) <$> Path.IO.getCurrentDir <*> (Path.parseRelDir "./audio_file")
+      (evalIO . makeM4bFromDir $ audioFileDirectory) `shouldReturn` True
+
   describe "andThenProcess" $ do
     it "can run pwd then date" $ do
       let pwd = System.Process.shell "pwd"
